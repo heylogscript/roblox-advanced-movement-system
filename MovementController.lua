b41631379411b45f1abfac0588f291958e42d84e
heylogscript <arthurasarzan@gmail.com>
2026-04-20 13:26:46 -0300

    Refactor movement state handling and improve slide momentum

--- MovementController.lua
+++ MovementController.lua
@@ -1,3 +1,5 @@
+-- Advanced movement system
+
 local Players = game:GetService("Players")
 local UserInputService = game:GetService("UserInputService")
 local RunService = game:GetService("RunService")
